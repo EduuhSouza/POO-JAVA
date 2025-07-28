75ceb3d872d8fa8f26a34e474eae281edcf471a5
EduuhSouza <146948069+EduuhSouza@users.noreply.github.com>
2025-07-28 17:04:58 -0300

    Add files via upload

---
+++ Carro.java
@@ -0,0 +1,26 @@
+public class Carro {
+    String nome;
+    int anoDeLancamento;
+    String marca;
+    String ipvaPago;
+    double somaDasAvaliacao;
+    int totalDeAvaliacao;
+
+    void exibeFichaTecnica(){
+        System.out.println("***********************\n");
+        System.out.println("Modelo do carro: " + nome);
+        System.out.println("Marca: " + marca);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+//        System.out.println("O IPVA está: " + ipvaPago);
+        System.out.println("\n***********************");
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacao += nota;
+        totalDeAvaliacao++;
+    }
+
+    double pegaMedia(){
+        return  somaDasAvaliacao / totalDeAvaliacao;
+    }
+}
---
+++ Principal.java
@@ -0,0 +1,49 @@
+public class Principal {
+    public static void main(String[] args) {
+//        Filme meuFilme = new Filme();
+//        meuFilme.nome = "AAAA";
+//        meuFilme.anoDeLancamento = 1900;
+//        meuFilme.duracaoEmMinutos = 180;
+//
+//        meuFilme.exibeFichaTecnica();
+//        meuFilme.avalia(8);
+//        meuFilme.avalia(5);
+//        meuFilme.avalia(10);
+//        System.out.println(meuFilme.somaDasAvaliacoes);
+//        System.out.println(meuFilme.totalAvaliacoes);
+//
+//        meuFilme.retornaMedia();
+//        System.out.println(meuFilme.retornaMedia());
+
+
+        //DESAFIO CARRO
+        Carro carro = new Carro();
+
+        carro.nome = "Mustangue";
+        carro.marca = "Ford";
+        carro.anoDeLancamento = 1969;
+
+        carro.exibeFichaTecnica();
+        carro.avalia(8);
+        carro.avalia(10);
+        carro.avalia(10);
+        System.out.println(carro.somaDasAvaliacao);
+        System.out.println(carro.totalDeAvaliacao);
+
+        carro.pegaMedia();
+        System.out.println(carro.pegaMedia());
+
+
+        // DESAFIO NOME
+//        Nome ficha = new Nome();
+//        ficha.nome = "Eduarda";
+//        ficha.idade = 20;
+//        ficha.corFavorita = "Preto";
+//        ficha.comidaFavorita = "Alaminuta";
+//        ficha.hobbie = "Jogar";
+//
+//        ficha.exibir();
+
+
+    }
+}
